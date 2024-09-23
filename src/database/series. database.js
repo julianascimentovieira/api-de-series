422fb947411b82dd60a055bc443bdd747c948a2a
Júlia Nascimento Vieira <157746817+julianascimentovieira@users.noreply.github.com>
2024-09-22 21:18:09 -0300

    Update series. database.js

--- src/database/series. database.js
+++ src/database/series. database.js
@@ -8,6 +8,7 @@ export const series = [
 	new Serie(5,'Doctor Who','Ficção Científica', 26),
 	new Serie(6,'Arcane','Ação/Animação', 1),
 	new Serie(7,'A Anatomia de Grey','Drama', 21),
+	new Serie(8,'Bridgerton','Romance', 3),
 	new Serie(9,'The Boys','Drama', 4), 
 	new Serie(10,'A Casa Do Dragão','Drama', 2), 
 	new Serie(11,'Suits','Drama', 9), 
